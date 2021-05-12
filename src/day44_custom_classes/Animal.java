8535641d04d9b68e2898bd93bb24bdb9199cd2c8
emrahgulum <82914905+emrahgulum@users.noreply.github.com>
2021-05-11 20:45:02 -0400

    added new line for try

---
+++ src/day44_custom_classes/Animal.java
@@ -0,0 +1,12 @@
+package day44_custom_classes;
+
+public class Animal {
+    String type = "Lion";
+
+    public void eat(){
+        System.out.println(type + " is eating");
+    }
+    public void speak(){
+        System.out.println(type + " can not speak");
+    }
+}
