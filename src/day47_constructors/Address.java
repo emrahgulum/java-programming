e74fce391c317432529791cfa0f3dd5b9462a6bf
emrahgulum <82914905+emrahgulum@users.noreply.github.com>
2021-05-27 21:40:56 -0400

    added constructors to Address.java and crated object in addreslist.java

--- src/day47_constructors/Address.java
+++ src/day47_constructors/Address.java
@@ -2,32 +2,29 @@ package day47_constructors;
 
 public class Address {
     private String street;
-    private String city ;
-    private String state ;
+    private String city;
+    private String state;
     private String zipCode;
     private String country = "USA";
-    // constructor
+    //constructor method- automatically called
     public Address() {
         System.out.println("Address constructor");
         street = "123 unknown st";
-        city = "unknown";
-        state = "unknown";
-        zipCode ="00000";
+        city = "Unknown";
+        state = "Unknown";
+        zipCode = "00000";
     }
-
+    //second constructor, overloaded constructor -> provides shortcut to initialize variables
+    //in same statement: ex: Address ad = new Address("123 java st", "Boston", "MA", "43213");
     public Address(String street, String city, String state, String zipCode) {
-        if(street.isEmpty() || street.length()>50){
-            System.out.println("ERROR: Invalid street");
-        }else {
-            this.street=street;
-        }
-
-        //setStreet(street);
-
+        setStreet(street);//reuse the code in the setter method
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
     }
 
     public String toString() {
-        return street + " , " + city + " , " + state + "  " + zipCode;
+        return street + ", " + city + ", " + state + " " + zipCode;
     }
 
     public String getStreet() {
@@ -35,7 +32,12 @@ public class Address {
     }
 
     public void setStreet(String street) {
-        this.street = street;
+        if(street.isEmpty() || street.length() > 50) {
+            System.out.println("ERROR: Invalid street");
+            //System.exit(0);
+        } else {
+            this.street = street;
+        }
     }
 
     public String getCity() {
